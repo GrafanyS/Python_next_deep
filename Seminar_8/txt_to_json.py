46ce0037cd84760e93c5529c225c9348a6fb23c7
Valetov Andrei <graf.any@gmail.com>
2023-06-11 23:22:50 +0300

    Seminar_8 сформировали текстовый файл с псевдо именами

--- Seminar_8/txt_to_json.py
+++ Seminar_8/txt_to_json.py
@@ -1,4 +1,5 @@
 """
+Задание №1
 Вспоминаем задачу 3 из прошлого семинара. Мы сформировали текстовый файл с псевдо именами и произведением чисел.
 Напишите функцию, которая создаёт из созданного ранее файла новый с данными в формате JSON. Имена пишите с большой
 буквы. Каждую пару сохраняйте с новой строки.
@@ -13,19 +14,11 @@ def txt_to_json(file: Path) -> None:
         for line in f:
             name, number = line.split(" ")
             file_data[name.capitalize()] = float(number)
-    with open(file.stem + '.json', 'w') as f:
-        json.dump(file_data, f, indent=2)
+    with open(file.stem + '.json', 'w') as f_json:
+        json.dump(file_data, f_json, indent=2)
 
 
-if __name__ == '__main__':
-    txt_to_json(Path('../Seminar_7/result'))
+# if __name__ == '__main__':
+#
+#     txt_to_json(Path('../Seminar_7/result'))
 
-
-# def txt_to_json(file: Path) -> None:
-#     file_data = {}
-#     with open(file, 'r', encoding='utf-8') as f:
-#         for line in f:
-#             name, number = line.split()
-#             file_data[name.capitalize()] = float(number)
-#     with open(file.stem + '.json', 'w', encoding='utf-8') as f_2:
-#         json.dump(file_data, f_2, ensure_ascii=False, indent=2)
