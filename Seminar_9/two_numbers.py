9d5ea5ea39442924b28f44e993f5b54d3dcc64f0
Valetov Andrei <graf.any@gmail.com>
2023-06-14 22:05:54 +0300

    Seminar_9 + two_numbers

--- Seminar_9/two_numbers.py
+++ Seminar_9/two_numbers.py
@@ -8,29 +8,9 @@ from typing import Callable
 
 
 def two_numbers(count_try: int, num: int) -> Callable[[], None]:
-    def random_numbers() -> None:
-        for i in range(1, count_try + 1):
-            user_input = input(' Введите число для отгадывания от 1 до 100: ')
-            if int(user_input) == num:
-                print(f'Вы угадали с {i} попытки')
-                break
-        else:
-            print('Вы не угадали')
-
-        return random_numbers()
-
-
-if __name__ == '__main__':
-
-    res = two_numbers(3, 20)
-    print(res)
-    res()
-
-
-def two_numbers(count_try: int, num: int) -> Callable[[], None]:
     def random_numbers():
         for i in range(1, count_try + 1):
-            user_input = input('Введите число для отгадывания от 1 до 100: ')
+            user_input = input(' Введите число для отгадывания от 1 до 100: ')
             if int(user_input) == num:
                 print(f'Вы угадали с {i} попытки')
                 break
@@ -40,5 +20,8 @@ def two_numbers(count_try: int, num: int) -> Callable[[], None]:
     return random_numbers
 
 
-res = two_numbers(3, 20)
-res()
+if __name__ == '__main__':
+
+    res = two_numbers(3, 20)
+    print(res)
+    res()
