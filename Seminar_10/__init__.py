180190faaed99edd86f564e1c4a9c151ee9b0d87
Valetov Andrei <graf.any@gmail.com>
2023-06-18 22:05:13 +0300

    Seminar_10 Домашнее задание init

--- Seminar_10/__init__.py
+++ Seminar_10/__init__.py
@@ -1,7 +1,11 @@
-import FunctionsToMethods
-import AnimalFabric
-import Animals
+from FunctionsToMethods import *
+from AnimalFabric import *
+from Animals import *
+from Human import *
 import Circle
 import CircleSet
 import Employee
-import Human
+
+
+__all__ = ["FeelNumbers", "NameGen", "TwoFilesInOne", "FunctionsToMethods", "JsonToCsv", "GetFromUser", "Fish",
+           "Bird", "Dog", "Animals", "Human"]
