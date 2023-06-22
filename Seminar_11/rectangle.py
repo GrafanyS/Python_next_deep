8601d3bfed2fcefef48e9284c4ab586f10e2aa9e
Valetov Andrei <graf.any@gmail.com>
2023-06-22 22:37:59 +0300

    Seminar_11 rectangle finale

--- Seminar_11/rectangle.py
+++ Seminar_11/rectangle.py
@@ -4,14 +4,28 @@
 
 
 class Rectangle:
+    """ We are finalizing the rectangle class from the last seminar. Add the ability to add and subtract. """
     def __init__(self, a: int, b: int = None):
+        """
+        initialize a and b
+        :param a:   -- a number
+        :param b:   -- b number
+        """
         self.a = a
         self.b = b if b is not None else a
 
     def perimeter(self):
+        """
+        calculate the perimeter
+        :return:    -- perimeter
+        """
         return 2 * (self.a + self.b)
 
     def area(self):
+        """
+        calculate the area rectangle
+        :return:    -- area
+        """
         return self.a * self.b
 
     def __add__(self, other):
@@ -27,7 +41,7 @@ class Rectangle:
         return Rectangle(new_a, new_b)
 
 
-if __name__ == '__main__':
+def run_rectangle():
     rect_1 = Rectangle(2, 5)
     rect_2 = Rectangle(5, 10)
     print(f'{rect_1.perimeter()= } {rect_1.area()= }')
@@ -36,3 +50,8 @@ if __name__ == '__main__':
     print(res_sum.a, res_sum.b)
     res_sub = rect_1 - rect_2
     print(res_sub.a, res_sub.b)
+    help(Rectangle)
+
+
+if __name__ == '__main__':
+    run_rectangle()
