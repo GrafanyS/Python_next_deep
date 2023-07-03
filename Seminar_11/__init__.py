7d437d02542ef2eac5752f5be47deb4894b01ccf
Valetov Andrei <graf.any@gmail.com>
2023-07-03 10:47:37 +0300

    Seminar_14 final

--- Seminar_11/__init__.py
+++ Seminar_11/__init__.py
@@ -3,4 +3,4 @@ from Python_next_deep.Seminar_11.archive import *
 from Python_next_deep.Seminar_11.rectangle import *
 from Python_next_deep.Seminar_11.matrix import *
 
-__all__ = ["my_str", "archive", "rectangle", "matrix"]
+__all__ = ["my_str", "archive", "rectangle", "matrix", "Matrix"]
